0ab8658749346c31f4d045d192932bcc3cdfc1e3
atordvairn <atordvairn@gmail.com>
2022-02-18 06:30:49 +0000

    tabs added

--- src/Dash.svelte
+++ src/Dash.svelte
@@ -6,10 +6,14 @@
         CardTitle,
         ListItemGroup,
         ListItem,
-        MaterialApp,
         Icon,
+        Tabs,
+        Tab,
+        Window,
+        WindowItem,
+        AppBar,
     } from "svelte-materialify";
-    import { mdiChat } from "@mdi/js";
+    import { mdiChatOutline, mdiDotsCircle } from "@mdi/js";
     import { user, username, db } from "./user.js";
 
     let items = JSON.parse(localStorage.getItem("items") || "[]");
@@ -42,47 +46,60 @@
 
         return mainData;
     }
+
+    let val = 0;
 </script>
 
 {#if $username}
-    <div>
-        <div class="display-2 m-2 text-center" />
-        <Card class="m-2">
-            <CardTitle class="h2">
-                <Icon path={mdiChat} size="30px" />
-                Chats
-            </CardTitle>
-            <CardText>
-                <ListItemGroup>
-                    <div>
-                        {#if !localStorage.getItem("items") || localStorage.getItem("items") == "[]"}
-                            <div class="text-center">
-                                <img
-                                    class="img-fluid"
-                                    src="images/empty-state-concept-3428212-2902554.png"
-                                    alt=""
-                                />
-                            </div>
-                            <div class="text-center m-3 h3">
-                                no rooms, yet! Go and make one !
-                            </div>
-                        {/if}
-                        {#each items as item, i (item)}
-                            {#await returnNull(item)}
-                                <ListItem>loading....</ListItem>
-                            {:then name}
-                                <a href={`/room?c=${item}`}>
-                                    <ListItem>
-                                        {name}
-                                    </ListItem>
-                                </a>
-                            {/await}
-                        {/each}
+    <AppBar>
+        <div slot="title">
+            Dashboard
+        </div>
+        <div slot="extension">
+            <Tabs class="green-text" bind:value={val} fixedTabs>
+                <div slot="tabs">
+                    <Tab>
+                        <Icon path={mdiChatOutline} />
+                        Chats
+                    </Tab>
+                    <Tab>
+                        <Icon path={mdiDotsCircle} />
+                        pērsona
+                    </Tab>
+                </div>
+            </Tabs>
+        </div>
+    </AppBar>
+    <Window value={val}>
+        <WindowItem>
+            <ListItemGroup class="p-3">
+                {#if !localStorage.getItem("items") || localStorage.getItem("items") == "[]"}
+                    <div class="text-center">
+                        <img
+                            class="img-fluid"
+                            src="images/empty-state-concept-3428212-2902554.png"
+                            alt=""
+                        />
                     </div>
-                </ListItemGroup>
-            </CardText>
-        </Card>
-    </div>
+                    <div class="text-center m-3 h3">
+                        no rooms, yet! Go and make one !
+                    </div>
+                {/if}
+                {#each items as item, i (item)}
+                    {#await returnNull(item)}
+                        <ListItem>loading....</ListItem>
+                    {:then name}
+                        <a href={`/room?c=${item}`}>
+                            <ListItem>
+                                {name}
+                            </ListItem>
+                        </a>
+                    {/await}
+                {/each}
+            </ListItemGroup>
+        </WindowItem>
+        <WindowItem>Persona</WindowItem>
+    </Window>
 {:else}
     <Login />
 {/if}
